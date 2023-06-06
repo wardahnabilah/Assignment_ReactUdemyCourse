2c49c2b08c072feef7e194c2c0e35a19e972aaf1
unknown <wardahnabilah33@gmail.com>
2023-06-06 16:26:06 +0700

    Create word count page

--- src/pages/WordCounter.js
+++ src/pages/WordCounter.js
@@ -1,7 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export function WordCounter() {
+  const [disabled, setDisabled] = useState(true)
+  const [result, setResult] = useState({characters: 0, words: 0})  
+
+  function handleChange(event) {
+    const inputValue = event.target.value
+
+    const wordsInput = (inputValue.trim()).split(' ')
+
+    setResult(prev => {
+      return {
+        ...prev,
+        characters: inputValue.length,
+        words: wordsInput.length
+      }
+    })
+
+    if(inputValue.length !== 0) {
+      setDisabled(false)
+    } else {
+      setResult(prev => {
+        return {...prev, words: 0}
+      })
+      setDisabled(true)
+    }
+  }
+
+  function clearInput() {
+    setResult({characters: 0, words: 0})
+  }
+  
   return (
-    <div>WordCounter</div>
+    <main className="px-6 py-12 text-center">
+      <h1 className="text-lg">Word Counter</h1>
+      <p className="mb-8 text-lg">Free online character and word count tool</p>
+      <form>
+        <textarea onChange={handleChange} className="w-[32rem] h-[16rem] pt-2 pl-3 border-2 border-slate-600" type="text" placeholder="Type or paste your text" />
+      </form>
+      <button onClick={clearInput} className="w-[6rem] h-[2.5rem] my-2 py-2 mx-auto text-white font-semibold bg-red-800 hover:bg-red-900 rounded-lg disabled:bg-slate-400 disabled:text-slate-500 disabled:cursor-not-allowed" disabled={disabled}>Clear</button>
+      <div className="p-4 flex justify-between">
+        <p className="w-[12rem] h-[2.5rem] py-2 mx-auto text-white font-semibold bg-blue-800 rounded-lg">Characters: {result.characters}</p>
+        <p className="w-[12rem] h-[2.5rem] py-2 mx-auto text-white font-semibold bg-blue-800 rounded-lg">Words: {result.words}</p>
+      </div>
+    </main>
   )
 }
